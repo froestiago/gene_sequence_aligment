db6adc713d03ee1170655f25b2621b3892eaadc0
Tiago Froes <froestiago94@gmail.com>
2023-11-21 17:10:18 -0300

    need to updated position on find_next_neighbor

--- SmithWatterman.py
+++ SmithWatterman.py
@@ -69,11 +69,56 @@ class SmithWatterman():
     
         return positions
 
+    # def __find_next_element(self, coord):
+    #     line = coord[0]
+    #     column = coord[1]
+    #     # sequence = self.matrix[line][column]
+    #     up_left = [(line - 1, column - 1), self.matrix[(line - 1,column - 1)]]
+    #     left = [(line, column -1), self.matrix[(line, column -1)]]
+    #     up = [(line -1, column), self.matrix[(line - 1, column)]]
+    #     positions_values = [up_left, left, up]
+
+    #     max_value = np.max(positions_values)
+    #     # max_value = np.max([up_left[1], left[1], up[1]])
+    #     print(f"max_value: {max_value}")
+
+
+    #     print(f"up_left -> coord: {up_left[0]} | value: {up_left[1]}")
+    #     print(f"left -> coord: {left[0]} | value: {left[1]}")
+    #     print(f"up -> coord: {up[0]} | value: {up[1]}")
+    #     # print(f"{up_left} - {left} - {up}")
+
+    def find_max_neighbor(self, coord):
+        
+        next_value = None
+        sequence = ""
+        while(next_value != 0):
+            # Get the indices for left, up, and diagonal-up-left
+            x, y = coord[0], coord[1]
+            left_index = (x, y - 1) if y > 0 else None
+            up_index = (x - 1, y) if x > 0 else None
+            diagonal_up_left_index = (x - 1, y - 1) if x > 0 and y > 0 else None
+
+            # Collect the indices in a list
+            indices_to_check = [index for index in [left_index, up_index, diagonal_up_left_index] if index is not None]
+
+            # Get values at the specified indices and calculate the maximum
+            values_to_check = [self.matrix[index] for index in indices_to_check]
+            max_value = np.max(values_to_check)
+
+            # Find the first occurrence of the maximum value in the list
+            max_position = indices_to_check[values_to_check.index(max_value)]
+            next_value = 0
+
+        return sequence
+        
+
     def get_align(self):
         alings = []
         max_position = self.__find_max_positions()
-        # for max_coord in max_position:
-
+        print(max_position)
+        for coord in max_position:
+            print(self.find_max_neighbor(coord))
 
 
 
@@ -81,5 +126,5 @@ lero = SmithWatterman(seq_1= 'WHAT',
                       seq_2= 'WHY')
 
 print(lero.matrix)
-max = lero.find_max_positions()
-print(max)
+max = lero.get_align()
+# print(max)
