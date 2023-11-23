eafd4fe3b5b6180796dea6abe2726746cdd4abfa
Tiago Froes <froestiago94@gmail.com>
2023-11-23 17:15:06 -0300

    SmithWatterman is almost complete... it actually needs to get the max out of the neighbors

--- SmithWatterman.py
+++ SmithWatterman.py
@@ -30,23 +30,23 @@ class SmithWatterman():
         return matrix[i-1][j] + self.gap_score
 
     def get_score(self, matrix, i, j):
-        print(f"comparing {self.seq_1[i]} - {self.seq_2[j]}")
+        # print(f"comparing {self.seq_1[i]} - {self.seq_2[j]}")
         if(self.seq_1[i] == self.seq_2[j]):
-            print(f"\tigual")
+            # print(f"\tigual")
             score_list = [self.__match_score(matrix, i, j),
                           self.__gap_horizontal_score(matrix, i, j),
                           self.__gap_vertical_score(matrix, i, j),
                           0]
         else:
-            print(f"\tdiferente")
+            # print(f"\tdiferente")
             score_list = [self.__mismatch_score(matrix, i, j),
                           self.__gap_horizontal_score(matrix, i, j),
                           self.__gap_vertical_score(matrix, i, j),
                           0]
 
             
-        print(f"\tscore_list: {score_list}")
-        print(f"\tscore: {np.max(score_list)}\n")
+        # print(f"\tscore_list: {score_list}")
+        # print(f"\tscore: {np.max(score_list)}\n")
         return np.max(score_list)
 
     def __fill_matrix(self):
@@ -68,63 +68,33 @@ class SmithWatterman():
         positions = [(index // shape[1], index % shape[1]) for index in flat_indices]
     
         return positions
-
-    # def __find_next_element(self, coord):
-    #     line = coord[0]
-    #     column = coord[1]
-    #     # sequence = self.matrix[line][column]
-    #     up_left = [(line - 1, column - 1), self.matrix[(line - 1,column - 1)]]
-    #     left = [(line, column -1), self.matrix[(line, column -1)]]
-    #     up = [(line -1, column), self.matrix[(line - 1, column)]]
-    #     positions_values = [up_left, left, up]
-
-    #     max_value = np.max(positions_values)
-    #     # max_value = np.max([up_left[1], left[1], up[1]])
-    #     print(f"max_value: {max_value}")
-
-
-    #     print(f"up_left -> coord: {up_left[0]} | value: {up_left[1]}")
-    #     print(f"left -> coord: {left[0]} | value: {left[1]}")
-    #     print(f"up -> coord: {up[0]} | value: {up[1]}")
-    #     # print(f"{up_left} - {left} - {up}")
-
-    def find_max_neighbor(self, coord):
-        
-        next_value = None
+    
+    def __get_local_align(self, coord):
         sequence = ""
-        while(next_value != 0):
-            # Get the indices for left, up, and diagonal-up-left
-            x, y = coord[0], coord[1]
-            left_index = (x, y - 1) if y > 0 else None
-            up_index = (x - 1, y) if x > 0 else None
-            diagonal_up_left_index = (x - 1, y - 1) if x > 0 and y > 0 else None
-
-            # Collect the indices in a list
-            indices_to_check = [index for index in [left_index, up_index, diagonal_up_left_index] if index is not None]
-
-            # Get values at the specified indices and calculate the maximum
-            values_to_check = [self.matrix[index] for index in indices_to_check]
-            max_value = np.max(values_to_check)
-
-            # Find the first occurrence of the maximum value in the list
-            max_position = indices_to_check[values_to_check.index(max_value)]
-            next_value = 0
-
-        return sequence
-        
+        current_value, next_value = None, None
+        x, y = coord[0], coord[1]
+        while(self.matrix[x][y] != 0):
+            sequence = sequence + self.seq_1[x]
+            # current_value, next_value = self.matrix[x][y], self.matrix[x-1][y-1]
+            x -= 1
+            y -= 1
+        return sequence[::-1]
 
     def get_align(self):
-        alings = []
+        aligns = []
         max_position = self.__find_max_positions()
-        print(max_position)
+        # print(f"max_position: {max_position}")
         for coord in max_position:
-            print(self.find_max_neighbor(coord))
+            # print(f"coord - {coord}")
+            aligns.append(self.__get_local_align(coord))
+
+        return aligns
 
 
 
-lero = SmithWatterman(seq_1= 'WHAT',
-                      seq_2= 'WHY')
+instance = SmithWatterman(seq_1= 'MTENSTSAAPAAKPKRAKAS', 
+                          seq_2= 'MTENSTSTPAAKPKRAKAS')
 
-print(lero.matrix)
-max = lero.get_align()
-# print(max)
+print(instance.matrix)
+sequences = instance.get_align()
+print(f"sequences: {sequences}")
